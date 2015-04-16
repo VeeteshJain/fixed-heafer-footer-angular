ef9d03dc95d5985c854680354f95c6281e00a571
veetesh <veeteshjain@gmail.com>
2015-04-16 09:20:52 +0530

    unit test cases.

--- tests/fixedHeaderFooterDir.spec.js
+++ tests/fixedHeaderFooterDir.spec.js
@@ -90,8 +90,70 @@ describe('fixedHeaderFooterTop Directive tests,', function (){
     });
   });
 it('should fixedHeaderFooterTop', function () {
-    //expect(topBottomDirectiveFactory.topBottomDirective).toBeDefined();
-    console.log(templateTop);
-    console.log(fixedHeaderFooterGroup.getInstance('top','oneTop'));
-  });
+  var topInstance = fixedHeaderFooterGroup.getInstance('top','oneTop');
+  var bottomInstance = fixedHeaderFooterGroup.getInstance('bottom','oneBottom');
+  expect(topInstance[1].Ids).toBeDefined();
+  expect(topInstance[1].Ids).toEqual(['oneTop']);
+  expect(bottomInstance[1].Ids).toBeDefined();
+  expect(bottomInstance[1].Ids).toEqual(['oneBottom']);
+});
+});
+
+describe('fixedHeaderFooter Directive tests,', function (){
+  console.log('describe');
+  var fixedHeaderFooterGroup,
+  topBottomDirectiveFactory,
+  scope,
+  element,
+  template,
+  optionsRef,
+  refreshCallbackRef,
+  timeout,
+  $window;
+  beforeEach(function(){
+    console.log('beforeEach');
+    angular.mock.module('ui.bootstrap.position');
+    angular.mock.module('fixedHeaderFooter');
+    inject(function($rootScope, $compile, $timeout, _$window_, _fixedHeaderFooterGroup_, _topBottomDirectiveFactory_){
+      timeout = $timeout;
+      $window = _$window_;
+      fixedHeaderFooterGroup = _fixedHeaderFooterGroup_;
+      topBottomDirectiveFactory = _topBottomDirectiveFactory_;
+      scope = $rootScope.$new();
+      scope.fixedHeaderFooterOne = {
+        topId: 'oneTop',
+        bottomId: 'oneBottom',
+        type: 'thead-tfoot',
+        onRegisterApi: function(options, refreshCallback){
+          optionsRef = options;
+          refreshCallbackRef = refreshCallback;
+        }
+      };
+      scope.tableOne = {
+        'height': 300,
+        'width': 300
+      };
+      spyOn(scope.fixedHeaderFooterOne, 'onRegisterApi').andCallThrough();
+      element = angular.element('<tablecontainer><div ng-style="{\'height\':tableOne.height+\'px\',\'width\':tableOne.width+\'px\'}"><table fixed-header-footer="fixedHeaderFooterOne"> <thead> <tr> <th>Month</th> <th>Savings</th> </tr></thead> <tbody> <tr> <td>January</td><td>$100</td></tr><tr> <td>February</td><td>$80</td></tr></tbody><tfoot> <tr> <td>Sum</td><td>$180</td></tr></tfoot></table></div></tablecontainer>');
+      template = $compile(element)(scope);
+      scope.$digest();
+    });
+});
+it('should compile.', function () {
+  expect(scope.fixedHeaderFooterOne.onRegisterApi).toHaveBeenCalled();
+  expect(scope.fixedHeaderFooterOne.onRegisterApi).toHaveBeenCalledWith(jasmine.any(Object), jasmine.any(Function));
+  refreshCallbackRef();
+});
+it('should exposes refreshCallback function.', function () {
+  refreshCallbackRef();
+});
+it('should unbind the events on scope destroy.', function () {
+  scope.$destroy();
+});
+it('should trigger refreshCallback after 100 clock unit.', function () {
+  timeout.flush();
+});
+it('should trigger refreshCallback on window resize.', function () {
+  $window.scroll();
+});
 });
